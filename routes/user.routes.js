bc14f2c51d4f0ba15a88f48a6d09b39581b015d5
Shamir Khan <shamirk2121@gmail.com>
2026-03-30 17:25:27 -0400

    feat: add user routes for retrieving user lists and individual profiles

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,20 +1,10 @@
 import { Router } from 'express';
+import { getCurrentUser, getUsers } from '../controllers/user.controller.js';
+import authorize from '../middlewares/auth.middleware.js';
 
 const userRouter = Router();
 
-// /api/v1/users
-userRouter.get('/', (req, res) => res.send({ title: 'GET all users' }));
-
-// /api/v1/users/:id
-userRouter.get('/:id', (req, res) => res.send({ title: 'GET user details' }));
-
-// /api/v1/users (POST)
-userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
-
-// /api/v1/users/:id (PUT)
-userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
-
-// /api/v1/users/:id (DELETE)
-userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
+userRouter.get('/', getUsers);
+userRouter.get('/:id', authorize, getCurrentUser);
 
 export default userRouter;
