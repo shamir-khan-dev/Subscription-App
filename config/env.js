c46a49fd084ed27673a0b7c2a16e16a847b95a39
Shamir Khan <shamirk2121@gmail.com>
2026-03-30 16:03:28 -0400

    feat: implement database connection logic with environment configuration and update morgan dependency

--- config/env.js
+++ config/env.js
@@ -5,8 +5,7 @@ config({ path: `config/.env.${process.env.NODE_ENV || 'development'}.local` });
 export const {
   PORT,
   NODE_ENV,
-  MONGODB_URI,
+  DB_URI,
   JWT_SECRET,
   JWT_EXPIRES_IN,
-  DB_PASSWORD,
 } = process.env;
--- database/mongodb.js
+++ database/mongodb.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
-import { MONGODB_URI, NODE_ENV } from '../config/env.js';
+import { DB_URI, NODE_ENV } from '../config/env.js';
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.development.local');
+if (!DB_URI) {
+  throw new Error('Please define the DB_URI environment variable inside .env.development.local');
 }
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(DB_URI);
     console.log(`📡 Connected to database in ${NODE_ENV} mode`);
   } catch (error) {
     console.error('❌ Error connecting to database:', error);
