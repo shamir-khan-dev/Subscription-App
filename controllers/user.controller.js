d60823f6296524a25814aec9c719cdff4576b64a
Shamir Khan <shamirk2121@gmail.com>
2026-03-30 17:24:11 -0400

    feat: implement user controller with methods to fetch all users and the current user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,18 @@
 import User from "../models/user.model.js";
 
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      success: true,
+      message: "Users fetched successfully",
+      data: users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const getCurrentUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.userId);
