27314f955dc3b6f6891e369f954c683ded61deef
Shamir Khan <shamirk2121@gmail.com>
2026-03-30 21:50:33 -0400

    feat: implement subscription controller with CRUD operations and workflow integration

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,6 @@
+import { workflowClient } from '../config/upstash.js';
 import Subscription from '../models/subscription.model.js';
+import { SERVER_URL } from '../config/env.js';
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -7,9 +9,20 @@ export const createSubscription = async (req, res, next) => {
       user: req.user._id,
     });
 
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        'content-type': 'application/json',
+      },
+      retries: 0,
+    });
+
     res.status(201).json({
       success: true,
-      data: subscription,
+      data: { subscription, workflowRunId },
     });
   } catch (error) {
     next(error);
